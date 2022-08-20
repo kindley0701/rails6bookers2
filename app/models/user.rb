562d26a86049cabf75b705a2b2ad49e6248ca25f
EC2 Default User <ec2-user@ip-172-31-62-54.ec2.internal>
2022-08-20 05:35:22 +0000

    add relation between users & room_users and messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments
   has_many :favorites
+  has_many :room_users
 
   # フォローをした、されたの関係
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
