6a91a11c50c35775f2141b71edab8af0ac0048bb
EC2 Default User <ec2-user@ip-172-31-62-54.ec2.internal>
2022-08-20 06:27:14 +0000

    finish 7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,7 +8,7 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   #検索方法ごとの分岐
   def self.looks(search, word)
     if search == "perfect_match"
@@ -23,5 +23,10 @@ class Book < ApplicationRecord
       @book = Book.all
     end
   end
+
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
 
 end
