5424394da7b236278a6977c51ebc18fe43ea3dbf
EC2 Default User <ec2-user@ip-172-31-62-54.ec2.internal>
2023-03-31 14:57:49 +0000

    add mailer

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -9,6 +9,10 @@ class NoticesController < ApplicationController
     @group = Group.find(params[:group_id])
     @notice.group_id = @group.id
     if @notice.save
+      @group.group_users.each do |group_user|
+        user = User.find(group_user.user_id)
+        ContactMailer.send_when_owner_announce(user, @group, @notice).deliver
+      end
       redirect_to group_notice_path(@group.id, @notice.id)
     else
       render :new
